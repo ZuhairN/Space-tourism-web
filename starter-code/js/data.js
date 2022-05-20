57f08cf655a031b12439f9530bd6080b96363f15
Zuhair.N <Zuhairnawasreh@gmail.com>
2022-05-20 18:53:09 +0300

    add mobile crew page with all tabs

--- starter-code/js/data.js
+++ starter-code/js/data.js
@@ -1,5 +1,5 @@
 const pages = ['destination', 'crew', 'technology'];
-const tabs = ['moon', 'mars', 'europa', 'titan'];
+const tabs = ['tab-1', 'tab-2', 'tab-3', 'tab-4'];
 let data;
 let page;
 let tab;
@@ -16,13 +16,12 @@ json().then((content) => {
 const tabList = document.querySelector('.tab-list');
 const img = document.querySelector('.img');
 const mainContent = document.querySelector('.content');
-const metaData = document.querySelector('.sub-content');
 const cont = mainContent.children;
-const meta = metaData.children;
 const btns = document.querySelectorAll('.btn-tabs');
 
 tabList.addEventListener('click', (e) => {
   const str = e.target.value;
+  console.log(str);
   if (e.target.getAttribute('class') === 'btn-tabs') {
     for (let i = 0; i < 4; i++) {
       btns[i].setAttribute('aria-selected', false);
@@ -33,15 +32,22 @@ tabList.addEventListener('click', (e) => {
         tab = i;
       }
     }
-    console.log(data[page][tab]);
     e.target.setAttribute('aria-selected', true);
-
-    img.src = `./assets/destination/image-${str}.webp`;
-    img.alt = `The ${data[page][tab].name}`;
-
-    cont[0].textContent = data[page][tab].name;
-    cont[1].textContent = data[page][tab].description;
-    meta[0].children[1].textContent = data[page][tab].distance;
-    meta[1].children[1].textContent = data[page][tab].travel;
+    const object = data[page][tab];
+    console.log(object);
+    if (page === 'destination') {
+      const meta = document.querySelector('.sub-content').children;
+      img.src = object.images.webp;
+      img.alt = `The ${object.name}`;
+      cont[0].textContent = object.name;
+      cont[1].textContent = object.description;
+      meta[0].children[1].textContent = object.distance;
+      meta[1].children[1].textContent = object.travel;
+    } else if (page === 'crew') {
+      img.src = object.images.webp;
+      img.alt = object.name;
+      cont[0].innerHTML = `<span>${object.role}</span><br/>${object.name}`;
+      cont[1].textContent = object.bio;
+    }
   }
 });
